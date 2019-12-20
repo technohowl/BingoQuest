6d4d145053196c437842974bfbebeb8d031f4f01
Tarun <ectarun@gmail.com>
2019-12-20 18:51:02 +0530

    Maps lock unlock based on bingos

--- src/app/game/scene/map-select.scene.ts
+++ src/app/game/scene/map-select.scene.ts
@@ -70,54 +70,54 @@ export class MapSelectScene extends StateContainer {
         containers.push(this.createLevel( 2,  3, new Point(  12, -145)));
         containers.push(this.createLevel( 3,  6, new Point( -89, -156)));
         containers.push(this.createLevel( 4,  9, new Point(-186, -189)));
-        containers.push(this.createLevel( 5, 12, new Point(-109, -251)));
-        containers.push(this.createLevel( 6, 16, new Point(   7, -256)));
-        containers.push(this.createLevel( 7, 20, new Point( 109, -272)));
-        containers.push(this.createLevel( 8, 24, new Point( 209, -325)));
-        containers.push(this.createLevel( 9, 28, new Point( 150, -380)));
-        containers.push(this.createLevel(10, 32, new Point(  36, -382)));
+        containers.push(this.createLevel( 5, 12, new Point(-109, -251),4));
+        containers.push(this.createLevel( 6, 16, new Point(   7, -256),4));
+        containers.push(this.createLevel( 7, 20, new Point( 109, -272),4));
+        containers.push(this.createLevel( 8, 24, new Point( 209, -325),4));
+        containers.push(this.createLevel( 9, 28, new Point( 150, -380),4));
+        containers.push(this.createLevel(10, 32, new Point(  36, -382),4));
 
-        containers.push(this.createLevel(11, 36, new Point( -63, -389)));
-        containers.push(this.createLevel(12, 40, new Point(-152, -413)));
-        containers.push(this.createLevel(13, 45, new Point(-176, -486)));
-        containers.push(this.createLevel(14, 50, new Point( -89, -503)));
-        containers.push(this.createLevel(15, 55, new Point(   7, -508)));
-        containers.push(this.createLevel(16, 60, new Point( 103, -520)));
-        containers.push(this.createLevel(17, 65, new Point( 189, -562)));
-        containers.push(this.createLevel(18, 70, new Point( 147, -626)));
-        containers.push(this.createLevel(19, 75, new Point(  60, -631)));
-        containers.push(this.createLevel(20, 80, new Point( -28, -641)));
+        containers.push(this.createLevel(11, 36, new Point( -63, -389),4));
+        containers.push(this.createLevel(12, 40, new Point(-152, -413),5));
+        containers.push(this.createLevel(13, 45, new Point(-176, -486),5));
+        containers.push(this.createLevel(14, 50, new Point( -89, -503),5));
+        containers.push(this.createLevel(15, 55, new Point(   7, -508),5));
+        containers.push(this.createLevel(16, 60, new Point( 103, -520),5));
+        containers.push(this.createLevel(17, 65, new Point( 189, -562),5));
+        containers.push(this.createLevel(18, 70, new Point( 147, -626),5));
+        containers.push(this.createLevel(19, 75, new Point(  60, -631),5));
+        containers.push(this.createLevel(20, 80, new Point( -28, -641),5));
 
-        containers.push(this.createLevel(21, 85, new Point(-118, -665)));
-        containers.push(this.createLevel(22, 90, new Point(-177, -716)));
-        containers.push(this.createLevel(23, 95, new Point(-162, -789)));
-        containers.push(this.createLevel(24, 100, new Point( -66, -792)));
-        containers.push(this.createLevel(25, 110, new Point(  37, -805)));
-        containers.push(this.createLevel(26, 120, new Point( 126, -829)));
-        containers.push(this.createLevel(27, 130, new Point( 191, -884)));
-        containers.push(this.createLevel(28, 140, new Point( 182, -940)));
-        containers.push(this.createLevel(29, 150, new Point(  90, -960)));
-        containers.push(this.createLevel(30, 160, new Point(  -5, -970)));
+        containers.push(this.createLevel(21, 85, new Point(-118, -665),5));
+        containers.push(this.createLevel(22, 90, new Point(-177, -716),5));
+        containers.push(this.createLevel(23, 95, new Point(-162, -789),5));
+        containers.push(this.createLevel(24, 100, new Point( -66, -792),10));
+        containers.push(this.createLevel(25, 110, new Point(  37, -805),10));
+        containers.push(this.createLevel(26, 120, new Point( 126, -829),10));
+        containers.push(this.createLevel(27, 130, new Point( 191, -884),10));
+        containers.push(this.createLevel(28, 140, new Point( 182, -940),10));
+        containers.push(this.createLevel(29, 150, new Point(  90, -960),10));
+        containers.push(this.createLevel(30, 160, new Point(  -5, -970),10));
 
-        containers.push(this.createLevel(31, 170, new Point( -97, -992)));
-        containers.push(this.createLevel(32, 180, new Point(-166,-1038)));
-        containers.push(this.createLevel(33, 190, new Point(-166,-1119)));
-        containers.push(this.createLevel(34, 200, new Point( -74,-1125)));
+        containers.push(this.createLevel(31, 170, new Point( -97, -992),10));
+        containers.push(this.createLevel(32, 180, new Point(-166,-1038),10));
+        containers.push(this.createLevel(33, 190, new Point(-166,-1119),10));
+        containers.push(this.createLevel(34, 200, new Point( -74,-1125),10));
 
-        containers.push(this.createLevel(35,210, new Point(  18,-1135)));
-        containers.push(this.createLevel(36,220, new Point( 116,-1150)));
-        containers.push(this.createLevel(37,230, new Point( 189,-1203)));
-        containers.push(this.createLevel(38,240, new Point( 118,-1253)));
-        containers.push(this.createLevel(39,250, new Point(  13,-1253)));
-        containers.push(this.createLevel(40,270, new Point( -85,-1270)));
-        containers.push(this.createLevel(41,290, new Point(-161,-1312)));
-        containers.push(this.createLevel(42,310, new Point(-173,-1392)));
-        containers.push(this.createLevel(43,330, new Point( -82,-1413)));
-        containers.push(this.createLevel(44,350, new Point(  25,-1414)));
-        containers.push(this.createLevel(45,370, new Point( 120,-1440)));
-        containers.push(this.createLevel(46,390, new Point( 183,-1495)));
-        containers.push(this.createLevel(47,420, new Point( 103,-1533)));
-        containers.push(this.createLevel(48,450, new Point(  18,-1533), true));
+        containers.push(this.createLevel(35,210, new Point(  18,-1135),10));
+        containers.push(this.createLevel(36,220, new Point( 116,-1150),10));
+        containers.push(this.createLevel(37,230, new Point( 189,-1203),10));
+        containers.push(this.createLevel(38,240, new Point( 118,-1253),10));
+        containers.push(this.createLevel(39,250, new Point(  13,-1253),20));
+        containers.push(this.createLevel(40,270, new Point( -85,-1270),20));
+        containers.push(this.createLevel(41,290, new Point(-161,-1312),20));
+        containers.push(this.createLevel(42,310, new Point(-173,-1392),20));
+        containers.push(this.createLevel(43,330, new Point( -82,-1413),20));
+        containers.push(this.createLevel(44,350, new Point(  25,-1414),20));
+        containers.push(this.createLevel(45,370, new Point( 120,-1440),20));
+        containers.push(this.createLevel(46,390, new Point( 183,-1495),30));
+        containers.push(this.createLevel(47,420, new Point( 103,-1533),30));
+        containers.push(this.createLevel(48,450, new Point(  18,-1533), 0,true));
 
 
         containers.sort( (a:ContainerComponent, b:ContainerComponent) => {
@@ -128,19 +128,19 @@ export class MapSelectScene extends StateContainer {
         return containers;
     }
 
-    createLevel(value:number, required:number, position:Point, isLast:boolean = false):ContainerComponent {
+    createLevel(value:number, required:number, position:Point, nextReq:number = 3, isLast:boolean = false):ContainerComponent {
 
         const locked:boolean = required > GameModelData.instance.bingos;
-        const areAllOpened:boolean = GameModelData.instance.bingos >= 449;
+        const areAllOpened:boolean = GameModelData.instance.bingos >= 450;
         if(!locked) {
             this.lastOpened = true;
         }
-        const isNextOpen:boolean = (required+3 > GameModelData.instance.bingos && !isLast);
+        const isNextOpen:boolean = ((required + nextReq) > GameModelData.instance.bingos && !isLast);
 //    TweenMax.from(image.scale, 0.2, {x: 0.1, y: 0.1, ease: Bounce.easeOut})
         const container:ContainerComponent = new ContainerComponent({
             element: {
                 position: position,
-                visible: true
+                visible: isNextOpen
             },
             children: [
                 new SpriteComponent({
